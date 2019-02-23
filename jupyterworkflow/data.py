3d51c6a7fe4ce8d9257c8e8a04952cb53f5a2be0
Bryce <bafeigum@gmail.com>
2019-02-22 21:29:57 -0800

    Refactor data download into the python module jupyterworkflow

---
+++ jupyterworkflow/data.py
@@ -0,0 +1,32 @@
+"""Module to import fremont data"""
+import os
+from urllib.request import urlretrieve
+
+import pandas as pd
+
+FREMONT_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+
+def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL, force_download=False):
+    """Download and cache the fremont data
+
+    Parameters
+    ~~~~~~~~~~
+    filename : string (optional)
+        location to save the data
+    url : string (optional)
+        web location of the data
+    force_download : bool (optional)
+        if True, force the redownload of the data
+
+    Returns
+    ~~~~~~~
+    data : pandas.DataFrame
+        The Fremont Bridge data
+    """
+
+    if force_download or not os.path.exists(filename):
+        urlretrieve(url, 'Fremont.csv')
+    data = pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
+    data.columns = ['West', 'East']
+    data['Total'] = data['West'] + data['East']
+    return data
